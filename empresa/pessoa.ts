e040e9fba6b897e08ef8bc9154e678b4aa4862a8
João Vitor Novais <joaoeuno2011@gmail.com>
2019-12-05 00:00:01 +0000

    adicionando classe Pessoa

---
+++ empresa/pessoa.ts
@@ -0,0 +1,30 @@
+namespace empresa {
+
+    export class Pessoa {
+        
+        private nome : string;
+        private dtNascimento : string;
+        private cpf : string;
+
+        public getNome() : string{ return this.nome; }
+        public setNome(nome:  string) : void {
+            if(nome != "") this.nome = nome;
+        }
+
+        public getDtNascimento(): string{ return this.dtNascimento; }
+        public setDtNascimento(dt: string){ this.dtNascimento = dt; }
+
+        public getIdade() : number {
+            
+            let now = new Date;
+            let anoAtual = now.getFullYear();
+            let anoNasc = this.dtNascimento.split('/')[2];
+            return Number(anoAtual) - Number(anoNasc);
+        }
+
+        public getCpf() : string { return this.cpf; }
+        public setCpf(cpf : string) : void {
+            if(cpf != "") this.cpf = cpf;
+        }
+    }
+}
